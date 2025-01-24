8daa054a13740b4f8f7a16abbbc6053d73960bdb
robertgruevski <robertgruevski@outlook.com>
2025-01-24 15:46:01 -0500

    Student and Course model. Beginning Db Context

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,7 +1,16 @@
-﻿namespace CollegeCourses.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeCourses.Models
 {
 	public class Course
 	{
-
+		public Course()
+		{
+			Students = new List<Student>();
+		}
+		public int CourseId { get; set; } // Primary Key and Identity
+		[Required]
+		public string? Title { get; set; }
+		public List<Student> Students { get; set; }
 	}
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -2,5 +2,13 @@
 {
 	public class Student
 	{
+		public Student()
+		{
+			Courses = new List<Course>();
+		}
+		public int StudentId { get; set; }
+		public string? Name { get; set; }
+		public string FinancialAidStatus { get; set; }
+		public List<Course> Courses { get; set; }
 	}
 }
